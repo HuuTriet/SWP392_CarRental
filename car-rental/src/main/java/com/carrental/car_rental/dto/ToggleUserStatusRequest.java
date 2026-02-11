ff2ee4b5f314c19aa82e3736c1ffb8e8a93194ec
huutriet <nhuutriet04@gmail.com>
2026-02-11 21:00:00 +0700

    chore: remove old Java Spring Boot source code

--- car-rental/src/main/java/com/carrental/car_rental/dto/ToggleUserStatusRequest.java
+++
@@ -1,9 +0,0 @@
-package com.carrental.car_rental.dto;
-
-import lombok.Data;
-
-// DTO cho việc chuyển đổi trạng thái người dùng (của hoàng)
-@Data
-public class ToggleUserStatusRequest {
-    private String reason; // Lý do chặn/mở chặn (optional)
-} 
