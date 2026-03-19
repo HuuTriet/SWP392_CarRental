e605eef58b4bc112f0c526ed421111a7482541b3
huutriet <nhuutriet04@gmail.com>
2026-03-19 21:08:50 +0700

    chore: remove old Java Spring Boot source code

--- car-rental/src/main/java/com/carrental/car_rental/repository/LanguageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carrental.car_rental.repository;
-
-import com.carrental.car_rental.entity.Language;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LanguageRepository extends JpaRepository<Language, String> {
-}
--- car-rental/src/main/java/com/carrental/car_rental/repository/RatingSummaryProjection.java
+++
@@ -1,7 +0,0 @@
-package com.carrental.car_rental.repository;
-
-public interface RatingSummaryProjection {
-    Integer getStars();
-    Integer getPercentage();
-    Integer getCount();
-}
